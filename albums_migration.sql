4aa189318aab684b9662536e2c4ee2a9dba4d0a8
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-03-01 12:04:10 -0600

    albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,7 +5,8 @@ CREATE TABLE quotes (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(50) NOT NULL,
                         album_name  VARCHAR(100) NOT NULL,
-                        release_date TEXT NOT NULL,
-
+                        release_date INTEGER NOT NULL,
+                        sales DOUBLE PRECISION NOT NULL,
+                        genre VARCHAR(50) NOT NULL,
                         PRIMARY KEY (id)
 );
