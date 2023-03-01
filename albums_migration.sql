3baa5ef5d410ac0c0ca218ab8dc9a71b3faf07f5
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-03-01 12:06:11 -0600

    albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,8 +4,8 @@ USE codeup_test_db;
 CREATE TABLE quotes (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(255),
-                        album_name  VARCHAR(100) NOT NULL,
-                        release_date INTEGER NOT NULL,
+                        album_name  VARCHAR(255),
+                        release_date INTEGER,
                         sales DOUBLE PRECISION NOT NULL,
                         genre VARCHAR(50) NOT NULL,
                         PRIMARY KEY (id)
