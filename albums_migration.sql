5885c601af885e70a746e5e45414bb78769f2c7a
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-03-03 10:22:35 -0600

    albums

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,7 +1,7 @@
 USE codeup_test_db;
 
 
-CREATE TABLE quotes (
+CREATE TABLE albums (
                         id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                         artist VARCHAR(255),
                         album_name  VARCHAR(255),
@@ -10,3 +10,16 @@ CREATE TABLE quotes (
                         genre VARCHAR(255),
                         PRIMARY KEY (id)
 );
+
+
+INSERT INTO albums (id, artist, album_name, release_date, sales, genre)
+VALUES (1, 'isaac', 'my album', 2004, 1000, 'rock');
+
+
+SELECT artist, album_name FROM albums;
+
+
+DESCRIBE ymir_employees.employees
+
+
+
