c073e6f43d65f5d169a985ccc4cb3f74dcb523ba
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-03-06 12:12:46 -0600

    second and third query

--- adlister.sql
+++ adlister.sql
@@ -1,13 +1,29 @@
+select *
+from adlister_users
+    join adlister_ads ads on adlister_users.id = ads.user_id
+    join ad_category ac on ads.id = ac.ad_id
+    join adlister_categories cat on cat.id = ac.category_id
+
+
 SELECT email
-from adlister_ads
-    inner join adlister_users au
-        on adlister_ads.user_id = au.id
-where adlister_ads.title = 'iphone'
-
-
-SELECT cat.name
 from adlister_ads as ads
-         inner join adlister_categories as cat
+    inner join adlister_users au
+        on ads.user_id = au.id
 where ads.title = 'iphone'
+
+
+SELECT name
+from adlister_categories as cat
+        inner join ad_category ac on cat.id = ac.category_id
+        inner join adlister_ads ads on ac.ad_id = ads.id
+where ads.title = 'desk'
+
+
+SELECT ads.title
+from adlister_categories as cat
+         inner join ad_category ac on cat.id = ac.category_id
+         inner join adlister_ads ads on ac.ad_id = ads.id
+where name = 'furniture'
+
 
 
